031a9f73c2f2e62776111946acfb5d445275ee7d
jradnan <mohd.yasin085208@gmail.com>
2022-06-19 21:42:32 +0600

    first commit

---
+++ src/Loginform.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqBu0502lZoMY0LGjmXcQJ52BneF2BWHk",
+  authDomain: "login-form-dfbd9.firebaseapp.com",
+  projectId: "login-form-dfbd9",
+  storageBucket: "login-form-dfbd9.appspot.com",
+  messagingSenderId: "440077642546",
+  appId: "1:440077642546:web:5156ecfa5baf3214aac52b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
